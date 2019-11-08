cd766cac87dc882f11f01157059eb836711724b7
Evgeniy Leutin <leutin.e@gmail.com>
2019-11-09 00:49:16 +0300

    Rename elements in ViewTwo

--- views/view_two.py
+++ views/view_two.py
@@ -5,26 +5,26 @@ from ..elements import ElementTwo
 
 
 class ViewTwo:
-    one_firstelement = ElementOne('name4', 5000)
-    one_secondelement = ElementOne('name5', 5000)
+    first = ElementOne('name4', 5000)
+    second = ElementOne('name5', 5000)
 
-    two_firstelement = ElementTwo('name6', 5000)
-    two_secondelement = ElementTwo('name7', 5000)
+    third = ElementTwo('name6', 5000)
+    fourth = ElementTwo('name7', 5000)
 
     class SubView:
-        two_firstelement = ElementTwo('name8', 5000)
-        two_secondelement = ElementTwo('name9', 5000)
+        fifth = ElementTwo('name8', 5000)
+        sixth = ElementTwo('name9', 5000)
 
         class SecondLevelOfSubView:
-            one_firstelement = ElementOne('name10', 5000)
+            seventh = ElementOne('name10', 5000)
 
     class SecondSubView:
-        one_firstelement = ElementOne('name11', 5000)
+        eighth = ElementOne('name11', 5000)
 
         class SecondLevelOfSecondSubView:
-            two_firstelement = ElementTwo('name12', 5000)
-            two_secondelement = ElementTwo('name13', 5000)
+            ninth = ElementTwo('name12', 5000)
+            tenth = ElementTwo('name13', 5000)
 
         class ThirdLevelOfSecondSubView:
-            one_firstelement = ElementOne('name14', 5000)
+            eleventh = ElementOne('name14', 5000)
 
